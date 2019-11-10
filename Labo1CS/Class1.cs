6b1f94e6ec1d885ac539ef908b4e8354d037c909
Tom Van de Weerdt <vandeweerdttom@gmail.com>
2019-11-10 18:52:57 +0100

    Add project files.

---
+++ Labo1CS/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo1CS
+{
+	class prog
+	{
+		static void Main(string[] args){
+			System.Console.WriteLine("Hello World!");
+			System.Console.WriteLine("Tom Van de Weerdt");
+			System.Console.WriteLine("2226");
+			System.Console.ReadLine();
+		}
+	}
+}
